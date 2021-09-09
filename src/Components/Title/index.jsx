20e6d13213141b27a3bdc59572e2d0dc17f7c1e4
HarlandLohora <harland@lohora.com>
2021-09-09 00:48:07 -0300

    clase01 completada

--- src/Components/Title/index.jsx
+++ src/Components/Title/index.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import styled from "styled-components"
 
-const Title = ({ children }) => {
-  return <h1 className="title">{children}</h1>;
-};
+const Title = styled.h1`
+  color: grey;
+  padding: 25px 0;
+`
+
 export default Title;
