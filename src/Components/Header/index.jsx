ecf55400e067acc49152a38494fff4359696bb81
Mari Spinola <mariana.spinola@alura.com.br>
2023-05-30 14:56:59 -0300

    fix: initialization

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 import logo from "../../assets/images/logo.svg";
 import { colorPrimario } from "../UI/variables";
 
